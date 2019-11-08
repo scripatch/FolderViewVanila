43da67b04057c7d946a7643650f1af66440cf8e8
scripatch <scripatch@gmail.com>
2019-11-08 09:39:56 +0300

    rev 0.9

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 "use strict";
 
+/**
+ * Объект DTO с настройками дерева папок
+ * @property {string} JSON_URL Строка с ссылкой к файлу JSON
+ * @property {string} appMountPoint Id элемента в DOM, в котором построится дерево папок
+ * @property {int} sortDirection Направление сортировки дерева папок: 1 - по возрастанию, -1 - по убыванию, 0 - без сортировки
+ * @property {string} filterStr Строка фильтрации названий папок
+ */
 const settings = {
     JSON_URL: "https://raw.githubusercontent.com/wrike/frontend-test/master/data.json",
     appMountPoint: "app",
@@ -7,10 +14,20 @@ const settings = {
     filterStr: "",
 };
 
+/**
+ * Объект сгенерированного дерева папок
+ * @property {Object} root Корневой элемент дерева папок
+ * @property {Map} nodesMap Соответствие id элементов с самими элементами, для быстрого доступа к элементу по id
+ * @property {Set} filteredNodesSet Набор id элементов, удоблетворяющих условиям фильтра
+ */
 const tree = {
     root: {},
     nodesMap: new Map(),
     filteredNodesSet: new Set(),
+
+    /**
+     * Инициализация дерева, обнуление корня и коллекций
+     */
     init() {
         this.root = {};
         this.nodesMap = new Map();
@@ -19,11 +36,21 @@ const tree = {
 
 };
 
+/**
+ * Основной объект приложения - "Визуализация папок"
+ * @property {Object} settings Настройки дерева папок
+ * @property {Array} data Массив исходных объектов - элементов дерева, полученный из JSON
+ * @property {Object} tree Сгенерированное дерево папок
+ */
 const appFolderView = {
     settings,
-    data: {},
+    data: [],
     tree,
 
+    /**
+     * Инициализация приложения, загрузка пользовательских настроек, получение данных, отрисовка основных элементов
+     * @param userSettings
+     */
     init(userSettings) {
 
         Object.assign(settings, userSettings);
@@ -44,24 +71,35 @@ const appFolderView = {
         }
     },
 
+    /**
+     * Отрисовка основных элементов управления и первая отрисовка самого дерево папок
+     */
     render() {
 
         this.getMountPoint().innerHTML =
-            '<button id="fv-sortInit"">Initial order</button> \
+            '<button id="fv-sortInit" class="fv-btn">Initial order</button> \
             <button id="fv-sortAZ" class="fv-btn">Sort A-Z</button> \
             <button id="fv-sortZA" class="fv-btn">Sort Z-A</button> \
-            <input id="fv-filter"><ul id="fv-treeRoot"></ul>';
+            <input id="fv-filter" class="fv-filter"><ul id="fv-treeRoot" class="fv-list fv-rootList"></ul>';
 
         this.renderTree();
 
     },
 
+    /**
+     * Отрисовка дерева папок
+     */
     renderTree() {
 
         this.buildTree();
         document.getElementById("fv-treeRoot").innerHTML = this.renderNode(this.tree.root);
     },
 
+    /**
+     * Формирование HTML строки для одного элемента дерева
+     * @param {Object} node Элемент дерева папок по которому формируется HTML строка
+     * @returns {string} Сформированная HTML строка
+     */
     renderNode(node) {
 
         if (!this.tree.filteredNodesSet.has(node.id)) return "";
@@ -69,7 +107,7 @@ const appFolderView = {
         let html = "<li class='fv-listItem'>" + node.title;
         if (node.children.length > 0) {
             html += "<ul class='fv-list'>";
-            html += node.children.reduce((acc, item) => acc += this.renderNode(item), "");
+            html += node.children.reduce((acc, item) => acc + this.renderNode(item), "");
             html += "</ul>";
         }
 
@@ -78,6 +116,9 @@ const appFolderView = {
 
     },
 
+    /**
+     * Установка основных обработчиков событий
+     */
     setEventHandlers() {
 
         document.getElementById("fv-sortInit").addEventListener('click', () => this.sort(0));
@@ -88,11 +129,19 @@ const appFolderView = {
 
     },
 
+    /**
+     * Сортировка дерева папок
+     * @param {int} sortDirection Направление сортировки
+     */
     sort(sortDirection) {
         this.settings.sortDirection = sortDirection;
         this.renderTree();
     },
 
+    /**
+     * Фильтрация дерева папок
+     * @param {string} str Строка фильтрации папок
+     */
     filter(str) {
         this.settings.filterStr = str;
         this.renderTree();
@@ -102,6 +151,13 @@ const appFolderView = {
         return document.getElementById(this.settings.appMountPoint);
     },
 
+    /**
+     * Построение дерева из исходных данных, с учетом сортировки и фильтрации
+     * При построении дерева используется коллекция, для хранения детей элемента, для того, чтобы сформировать дерево
+     * с одного прохода
+     * Если найден элемент с id=-1 он назначается корневым элементом
+     * Параллельно заполняем коллекцию содержащюю все id всех элементов
+     */
     buildTree() {
 
         let childrenMap = new Map();
@@ -134,6 +190,10 @@ const appFolderView = {
 
     },
 
+    /**
+     * Фильтрация сгенерированного дерева папок
+     * В случае если папка соответствует фильтру, то все его родители также помечаются как соответствующие фильтру.
+     */
     filterTree() {
         this.tree.nodesMap.forEach((node, id) => {
             if (!this.tree.filteredNodesSet.has(id) && this.filterNode(node)) {
@@ -147,15 +207,24 @@ const appFolderView = {
         });
     },
 
+    /**
+     * Функция определяет соответствует ли элемент фильтру
+     * @param {Object} node Проверяемый элемент дерева папок
+     * @returns {boolean} Истина - соответствует фильтру, ложь - нет
+     */
     filterNode(node) {
         return node.title.toLowerCase().includes(this.settings.filterStr.toLowerCase());
     },
 
-
+    /**
+     * Обработчик нажатия на элемент дерева папок, чтобы открыть или спрятать вложенные элементы
+     * @param event Событие нажатия
+     */
     clickHandler(event) {
         let target = event.target;
         if (target.classList.contains('fv-listItem') && target.firstElementChild && target.firstElementChild.nodeName === "UL") {
             target.firstElementChild.classList.toggle('hidden');
+            target.classList.toggle('closed');
         }
     },
 
